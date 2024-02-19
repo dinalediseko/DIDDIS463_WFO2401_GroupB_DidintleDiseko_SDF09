1b282462676edf9c4cb47056263961d243857ef4
dinalediseko <99658820+dinalediseko@users.noreply.github.com>
2024-02-19 12:01:25 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class',
+  theme: {
+      extend: {
+          colors: {
+              teal: {
+                  '600': 'rgb(15, 118, 110)',
+                  '950': 'rgb(4, 47, 46)',
+              },
+              stone: {
+                  '300': 'rgb(214, 211, 209)',
+                  '900': 'rgb(28, 25, 23)',
+              },
+              orange: {
+                  '600': 'rgb(234, 88, 12)',
+                  '700': 'rgb(194, 65, 12)',
+              },
+          },
+      },
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+}
